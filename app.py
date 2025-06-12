756ee4d5fa8ccd62c47203c01750b22e7dfd2fea
declanrjb <declanrjb@gmail.com>
2025-06-12 16:41:46 -0400

    npm be gone

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request
+import os
+
+from flask import Flask, jsonify, request, Response
+import math
+import itertools
+import flask_csv
+import pandas as pd
+import re
+
+app = Flask(__name__)
+
+# no modification required beyond function name
+@app.route('/transcribe')
+def transcribe():
+
+    file = request.args['file']
+
+    r = Response(jsonify({
+        'file': file
+    }), mimetype='application/json')
+    
+    r.headers.add('Access-Control-Allow-Origin', '*')
+    return r
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- transcribe.js
+++ transcribe.js
@@ -1,84 +1,9 @@
 
+import { ElevenLabsClient } from "elevenlabs";
 
-/*
 const client = new ElevenLabsClient({ apiKey: "sk_412c599fb0a33891e818696239ecc14250afd1c2c8ae32f1" });
+
 await client.speechToText.convert({
-    modelId: "scribe_v1"
+	model_id: "scribe_v1",
+	file: file,
 });
-*/
-
-
-$(function() {
-
-  
-
-  $('#upload-audio').on('click', function() {
-    $('#audio-input').click()
-  })
-
-  $('#audio-input').on('change', function(e) {
-    var file = e.target.files[0];
-
-    const reader = new FileReader();
-    reader.onload = async (e) => {
-
-        console.log(e.target.result)
-        const audioBlob = new Blob([e.target.result], { type: "audio/mp3" });
-        console.log(audioBlob)
-
-        console.log(e)
-        console.log(new File([audioBlob], 'audio.mp3'))
-        console.log('sending...')
-
-        const formData = new FormData();
-        formData.append('file', e.target.result);
-        formData.append('model_id', 'scribe_v1')
-    
-        fetch('https://api.elevenlabs.io/v1/speech-to-text', {
-          method: 'POST',
-          headers: {
-            'xi-api-key': 'sk_412c599fb0a33891e818696239ecc14250afd1c2c8ae32f1',
-            'content-type': 'multipart/form-data'
-          },
-          body: {
-            'model_id': 'scribe_v1',
-            'file': e.target.result
-          }
-        });
-              
-    };
-    reader.readAsDataURL(file);
-    
-  })
-})
-
-
-
-
-/*
-// example.mts
-import { ElevenLabsClient } from "./node_modules/@elevenlabs/elevenlabs-js/index.js";
-import "./node_modules/dotenv/config.js";
-
-const elevenlabs = new ElevenLabsClient();
-
-const response = await fetch(
-  "https://storage.googleapis.com/eleven-public-cdn/audio/marketing/nicole.mp3"
-);
-const audioBlob = new Blob([await response.arrayBuffer()], { type: "audio/mp3" });
-
-const transcription = await elevenlabs.speechToText.convert({
-  file: audioBlob,
-  modelId: "scribe_v1", // Model to use, for now only "scribe_v1" is supported.
-  tagAudioEvents: true, // Tag audio events like laughter, applause, etc.
-  languageCode: "eng", // Language of the audio file. If set to null, the model will detect the language automatically.
-  diarize: true, // Whether to annotate who is speaking
-});
-
-console.log(transcription);
-*/
-
-
-/*
-
-*/
