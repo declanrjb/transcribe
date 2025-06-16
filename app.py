1db791c7e34f93dd41864194fbdd224637d613cd
declanrjb <declanrjb@gmail.com>
2025-06-16 12:09:50 -0400

    test directly

--- app.py
+++ app.py
@@ -8,15 +8,31 @@ import flask_csv
 import pandas as pd
 import re
 import json
+from elevenlabs.client import ElevenLabs
 
 app = Flask(__name__)
 
 # no modification required beyond function name
 @app.route('/transcribe', methods=['GET', 'POST'])
 def transcribe():
+    #file = request.files[0]
+
+    elevenlabs = ElevenLabs(
+        api_key=os.getenv("ELEVENLABS_API_KEY"),
+    )
+
+    transcription = elevenlabs.speech_to_text.convert(
+        file=request.files[0],
+        model_id="scribe_v1", # Model to use, for now only "scribe_v1" is supported
+        tag_audio_events=True, # Tag audio events like laughter, applause, etc.
+        language_code="eng", # Language of the audio file. If set to None, the model will detect the language automatically.
+        diarize=True, # Whether to annotate who is speaking
+    )
+
 
     r = Response(json.dumps({
-        'request': request.form
+        'request': request.form,
+        'response': transcription
     }), mimetype='application/json')
 
     r.headers.add('Access-Control-Allow-Origin', '*')
